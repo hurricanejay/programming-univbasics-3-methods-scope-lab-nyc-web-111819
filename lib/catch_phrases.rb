8c58e45cdd50b53dde0c5fd33eeec9685243e77c
Jay Wen <jaywen89@gmail.com>
2019-11-06 21:58:07 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -11,6 +11,6 @@ def link
   puts status = "It's Dangerous To Go Alone! Take This."
 end
 
-def any_phrase("takes in an argument and puts out the catch phrase")
+def any_phrase(phrase)
   puts = 'Do A Barrel Roll!'
 end
