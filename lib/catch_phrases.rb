eb5cb639b002f8dbc82fae3a9f8fb2ad91c255df
Jay Wen <jaywen89@gmail.com>
2019-11-06 21:30:15 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -6,12 +6,3 @@ end
 def toadstool
   puts status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 end
-
-def link
-  puts status = 'It's Dangerous To Go Alone! Take This.\n'
-end
-
-def any_phrase
-  status = 'takes in an argument and puts out the catch phrase'
-  puts = 'Do A Barrel Roll!'
-end
